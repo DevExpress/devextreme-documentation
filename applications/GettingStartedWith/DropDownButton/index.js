747419f5ce5317d3be0f410143c1ad1842739602
Anton Chernous <74176530+czernous-devexpress@users.noreply.github.com>
2021-03-04 19:54:48 +0300

    Add Customize DropDown menu article to Getting Started guides (#1946)
    * Add Customize DropDown menu article to Getting Started guides
    
    * Update common-customize-dropdown-menu.md
    
    * Update includes/common-customize-dropdown-menu.md
    
    Co-authored-by: RomanTsukanov <RomanTsukanov@users.noreply.github.com>
    
    * Add code snippets
    
    * Update code snipptes, remove cutstomize drop-down menu article from the DateBox tutorial
    
    * Add minor fixes
    
    * Update example, apply Andrew's suggestions
    
    * Update formatting
    
    * Update includes/common-customize-dropdown-menu.md
    
    Co-authored-by: RomanTsukanov <RomanTsukanov@users.noreply.github.com>
    
    Co-authored-by: RomanTsukanov <RomanTsukanov@users.noreply.github.com>
    Co-authored-by: RomanTsukanov <roman.tsukanov@devexpress.com>

--- applications/GettingStartedWith/DropDownButton/index.js
+++ applications/GettingStartedWith/DropDownButton/index.js
@@ -1,23 +1,26 @@
-$(function() {
+$(function () {
     var actions = [
-        { id: 1, text: "My profile", icon: "user" },
-        { id: 2, text: "Messages", icon: "email" },
-        { id: 3, text: "Contacts", icon: "group" },
-        { id: 4, text: "Log out", icon: "runner" }
+        { id: 1, text: 'My profile', icon: 'user' },
+        { id: 2, text: 'Messages', icon: 'email' },
+        { id: 3, text: 'Contacts', icon: 'group' },
+        { id: 4, text: 'Log out', icon: 'runner' }
     ];
 
-    $("#myDropDownButton").dxDropDownButton({
-        text: "Sandra Johnson",
-        icon: "user",
+    $('#myDropDownButton').dxDropDownButton({
+        text: 'Sandra Johnson',
+        icon: 'user',
         items: actions,
-        
-        displayExpr: "text",
-        onItemClick: function(e) {
-            console.log(e.itemData.text + " was clicked");
+
+        displayExpr: 'text',
+        onItemClick: function (e) {
+            console.log(e.itemData.text + ' was clicked');
         },
         splitButton: true,
-        onButtonClick: function() {
-            console.log("Main button was clicked");
+        onButtonClick: function () {
+            console.log('Main button was clicked');
+        },
+        dropDownOptions: {
+            height: 150
         }
     });
 });

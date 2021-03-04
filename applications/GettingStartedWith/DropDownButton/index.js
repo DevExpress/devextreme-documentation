f3b288d938a1eb5771f51e54c41af75b741dd751
czernous-devexpress <anton.chernous@devexpress.com>
2021-03-04 14:31:24 +0300

    Update formatting

--- applications/GettingStartedWith/DropDownButton/index.js
+++ applications/GettingStartedWith/DropDownButton/index.js
@@ -1,26 +1,26 @@
 $(function () {
-  var actions = [
-    { id: 1, text: 'My profile', icon: 'user' },
-    { id: 2, text: 'Messages', icon: 'email' },
-    { id: 3, text: 'Contacts', icon: 'group' },
-    { id: 4, text: 'Log out', icon: 'runner' },
-  ];
+    var actions = [
+        { id: 1, text: 'My profile', icon: 'user' },
+        { id: 2, text: 'Messages', icon: 'email' },
+        { id: 3, text: 'Contacts', icon: 'group' },
+        { id: 4, text: 'Log out', icon: 'runner' }
+    ];
 
-  $('#myDropDownButton').dxDropDownButton({
-    text: 'Sandra Johnson',
-    icon: 'user',
-    items: actions,
+    $('#myDropDownButton').dxDropDownButton({
+        text: 'Sandra Johnson',
+        icon: 'user',
+        items: actions,
 
-    displayExpr: 'text',
-    onItemClick: function (e) {
-      console.log(e.itemData.text + ' was clicked');
-    },
-    splitButton: true,
-    onButtonClick: function () {
-      console.log('Main button was clicked');
-    },
-    dropDownOptions: {
-      height: 150,
-    },
-  });
+        displayExpr: 'text',
+        onItemClick: function (e) {
+            console.log(e.itemData.text + ' was clicked');
+        },
+        splitButton: true,
+        onButtonClick: function () {
+            console.log('Main button was clicked');
+        },
+        dropDownOptions: {
+            height: 150
+        }
+    });
 });

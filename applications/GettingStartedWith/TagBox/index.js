4523a61a8d4971193f11d51d264d6b056780f98b
Alexander Yakovlev <33040934+AlexJameson@users.noreply.github.com>
2021-07-05 14:14:35 +0300

    Add Getting Started with Tagbox 21.2 (#2526)
    * Add Getting Started with Tagbox 21.2
    
    * fix version
    
    * fix includes
    
    Co-authored-by: Alexander Yakovlev <alexander.yakovlev@devexpress.com>

---
+++ applications/GettingStartedWith/TagBox/index.js
@@ -0,0 +1,90 @@
+$(function() {
+    const dataSource = new DevExpress.data.DataSource({
+        store: data,
+        key: "ID",
+        group: "Category"
+    });
+    
+    $("#tagBox").dxTagBox({
+        id: "tagBox",
+        dataSource: dataSource,
+        valueExpr: "ID",
+        displayExpr: "Name",
+        opened: true,
+        showSelectionControls: true,
+        searchEnabled: true,
+        grouped: true,
+        multiline: true,
+        maxDisplayedTags: 6,
+        onValueChanged: function(e) {
+            console.log(e.previousValue);
+            console.log(e.value);
+        },
+        dropDownOptions: {
+            maxHeight: 300
+        }
+    });
+    
+});
+
+const data = [{
+    "ID": 1,
+    "Name": "HD Video Player",
+    "Category": "Video Players"
+}, {
+    "ID": 2,
+    "Name": "SuperHD Player",
+    "Category": "Video Players"
+}, {
+    "ID": 3,
+    "Name": "SuperPlasma 50",
+    "Category": "Televisions"
+}, {
+    "ID": 4,
+    "Name": "SuperLED 50",
+    "Category": "Televisions"
+}, {
+    "ID": 7,
+    "Name": "SuperLCD 42",
+    "Category": "Televisions"
+}, {
+    "ID": 8,
+    "Name": "SuperPlasma 65",
+    "Category": "Televisions"
+}, {
+    "ID": 10,
+    "Name": "DesktopLED 21",
+    "Category": "Monitors"
+}, {
+    "ID": 11,
+    "Name": "DesktopLED 19",
+    "Category": "Monitors"
+}, {
+    "ID": 12,
+    "Name": "DesktopLCD 21",
+    "Category": "Monitors"
+}, {
+    "ID": 13,
+    "Name": "DesktopLCD 19",
+    "Category": "Monitors"
+}, {
+    "ID": 14,
+    "Name": "Projector Plus",
+    "Category": "Projectors"
+}, {
+    "ID": 15,
+    "Name": "Projector PlusHD",
+    "Category": "Projectors"
+}, {
+    "ID": 16,
+    "Name": "Projector PlusHT",
+    "Category": "Projectors"
+}, {
+    "ID": 17,
+    "Name": "ExcelRemote IR",
+    "Category": "Automation"
+}, {
+    "ID": 18,
+    "Name": "ExcelRemote BT",
+    "Category": "Automation"
+}];
--- concepts/05 UI Components/TagBox/00 Getting Started with TagBox/40 Handle the Value Change Event.md
+++ concepts/05 UI Components/TagBox/00 Getting Started with TagBox/40 Handle the Value Change Event.md
@@ -1,5 +1,3 @@
 #include tutorials-editors-handle-value-change with {
     output: "the IDs of the currently and previously selected items"
 }
-
-Next, we will group the drop-down items.

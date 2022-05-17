5d5d3bd98a3090d79faf3ab2a00a16db3ffd92e1
skorokhodova.vlada <vlada.skorokhodova@devexpress.com>
2022-05-17 12:10:41 +0300

    Change code

--- applications/GettingStartedWith/Toast/index.js
+++ applications/GettingStartedWith/Toast/index.js
@@ -4,17 +4,18 @@ $(function () {
     $("#show").dxButton({
         text: "Show message",
         onClick: function() {
-            DevExpress.ui.notify({
-                message: "You have a new message", 
-                width: 230,
-                position: {
-                    my: "bottom",
-                    at: "bottom",
-                    of: "#container"
-                }
-            }, 
-            types[Math.floor(Math.random() * 4)], 
-            500
+            DevExpress.ui.notify(
+                {
+                    message: "You have a new message", 
+                    width: 230,
+                    position: {
+                        my: "bottom",
+                        at: "bottom",
+                        of: "#container"
+                    }
+                }, 
+                types[Math.floor(Math.random() * 4)], 
+                500
             );
         },
     });
@@ -22,20 +23,24 @@ $(function () {
     $("#custom").dxButton({
         text: "Show custom message",
         onClick: function() {
-            DevExpress.ui.notify({
-                width: 230,
-                height: 50,
-                position: {
-                    my: "bottom",
-                    at: "bottom",
-                    of: "#container"
-                },
-                contentTemplate: function () {
-                    return $("<p />").text("You have a new message ").addClass("custom-toast").append('<i class="dx-icon-email"></i>');
-                }
-            }, 
-            "custom", 
-            500
+            DevExpress.ui.notify(
+                {
+                    width: 230,
+                    height: 50,
+                    position: {
+                        my: "bottom",
+                        at: "bottom",
+                        of: "#container"
+                    },
+                    contentTemplate: function () {
+                        return $("<p />")
+                        .text("You have a new message ")
+                        .addClass("dx-custom-toast")
+                        .append('<i class="dx-icon-email"></i>');
+                    }
+                }, 
+                "custom", 
+                500
             );
         },
     });

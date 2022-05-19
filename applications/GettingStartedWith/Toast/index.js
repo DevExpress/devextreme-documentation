e8b2a488d64d1c3e65902e7a91a6671883f427b8
Vlada Skorohodova <94827090+vladaskorohodova@users.noreply.github.com>
2022-05-19 11:44:04 +0300

    Toast: add a Getting Started tutorial (#3711)
    (cherry picked from commit b7881d1632d65c3a39b256432b9962432f31c92d)

---
+++ applications/GettingStartedWith/Toast/index.js
@@ -0,0 +1,46 @@
+$(function () {
+    const types = ['error', 'info', 'success', 'warning'];
+
+    $("#show-message").dxButton({
+        text: "Show message",
+        onClick: function() {
+            DevExpress.ui.notify(
+                {
+                    message: "You have a new message", 
+                    width: 230,
+                    position: {
+                        my: "bottom",
+                        at: "bottom",
+                        of: "#container"
+                    }
+                }, 
+                types[Math.floor(Math.random() * 4)], 
+                500
+            );
+        },
+    });
+
+    $("#show-custom-message").dxButton({
+        text: "Show custom message",
+        onClick: function() {
+            DevExpress.ui.notify(
+                {
+                    width: 230,
+                    height: 50,
+                    position: {
+                        my: "bottom",
+                        at: "bottom",
+                        of: "#container"
+                    },
+                    contentTemplate: (element) => {
+                        element.append('<p>You have a new message</p> &nbsp;');
+                        element.append('<i class="dx-icon-email icon-style"></i>');
+                    }
+                }, 
+                "custom", 
+                500
+            );
+        },
+    });
+
+});

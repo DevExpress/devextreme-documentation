8e086c3ec5459e94bdfb162f3ddc2a28312c4654
CORP\serhii.pylypchuk <serhii.pylypchuk@devexpress.com>
2022-12-23 18:20:26 +0400

    Switch: update variables in code snippets

--- applications/GettingStartedWith/Switch/index.js
+++ applications/GettingStartedWith/Switch/index.js
@@ -5,11 +5,11 @@ $(() => {
         rtlEnabled: true,
         hint: "Click to switch on",
         onValueChanged(e) {
-            const message_text= e.value ? "Click to switch off" : "Click to switch on";
-            e.component.option("hint", message_text);
+            const messageText= e.value ? "Click to switch off" : "Click to switch on";
+            e.component.option("hint", messageText);
             
-            const state_label = e.value ? "ON" : "OFF";
-            DevExpress.ui.notify(`The component is switched ${state_label}`); 
+            const stateLabel = e.value ? "ON" : "OFF";
+            DevExpress.ui.notify(`The component is switched ${stateLabel}`); 
         }
     });
 });

2df8be5cf6a8cabf52684b3cde7969a9c36684ff
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-06-23 15:36:52 +0400

    FileUploader: Add Customize FileUploader How To (#7488)

---
+++ applications/UIWidgets/FileUploader/Customization/index.js
@@ -0,0 +1,18 @@
+$(() => {
+    const fileUploader = $('#file-uploader').dxFileUploader({
+        dropZone: '.file-uploader-block',
+        uploadMode: 'useButtons',
+        multiple: true,
+        allowedFileExtensions: ['.jpg', '.jpeg', '.gif', '.png'],
+    }).dxFileUploader("instance");
+
+    const fileOne = new File([new Blob(["testing"])], "Image One.jpg", { type: "text/plain" });
+    const fileTwo = new File([new Blob(["testing"])], "Image Two.jpg", { type: "text/plain" });
+    const fileThree = new File([new Blob(["testing"])], "Image Three.jpg", { type: "text/plain" });
+    const fileFour = new File([new Blob(["testing"])], "Image Four.jpg", { type: "text/plain" });
+    const fileFive = new File([new Blob(["testing"])], "Image Five.jpg", { type: "text/plain" });
+
+    fileUploader.option("value", [fileOne, fileTwo, fileThree, fileFour, fileFive]);
+    //fileUploader.upload();
+});
+

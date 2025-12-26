dbdce362c7f30c7344226d2acbcac03ecff36912
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-26 09:36:01 +0400

    Copy Styles to iframe to Eliminate XSS Risk

--- applications/ComponentList/index.js
+++ applications/ComponentList/index.js
@@ -1,21 +1,7 @@
-const iframeSheets = document.styleSheets;
-
-Object.entries(iframeSheets).forEach(([key, sheet]) => {
+Object.entries(document.styleSheets).forEach(([key, sheet]) => {
     if (sheet.href?.includes('fluent')) {
-        iframeSheets[key].disabled = true;
+        document.styleSheets[key].disabled = true;
     }
 });
 
-const parentSheets = window.parent.document.styleSheets;
-
-Object.values(parentSheets).forEach(sheet => {
-    const href = sheet.href?.toLowerCase() || '';
-    const frameworks = ['jquery', 'angular', 'vue', 'react'];
-
-    if (frameworks.some(fw => href.includes(fw))) {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = sheet.href;
-        document.head.appendChild(link);
-    }
-});
+document.body.classList.add(localStorage.getItem('dx-devextreme-site-framework'));

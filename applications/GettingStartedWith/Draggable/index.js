e4263d6309da201d62ae7b79344ed3508f54b19d
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-26 14:17:33 +0400

    Add DragMove and DragEnd Handlers

--- applications/GettingStartedWith/Draggable/index.js
+++ applications/GettingStartedWith/Draggable/index.js
@@ -1,38 +1,71 @@
 $(() => {
-  
+    const notes = ["#note-1", "#note-2", "#note-3", "#note-4"];
     let z = 0;
 
-    $('#note-1').dxDraggable({
-        onDragStart: handleDragStart,
-        boundary: '.board',
-    }).on('click', handleClick);
-    
-    $('#note-2').dxDraggable({
-        onDragStart: handleDragStart,
-        boundary: '.board',
-    }).on('click', handleClick);
-    
-    $('#note-3').dxDraggable({
-        onDragStart: handleDragStart,
-        boundary: '.board',
-    }).on('click', handleClick);
-    
-    $('#note-4').dxDraggable({
-        onDragStart: handleDragStart,
-        boundary: '.board',
-    }).on('click', handleClick);
+  let handleComponent;
+  $("#note-1")
+    .dxDraggable({
+      onDragStart: handleDragStart,
+      onDragMove: handleDragMove,
+      onDragEnd: handleDragEnd,
+      boundary: ".board",
+      group: "1",
+    })
+    .on("click", handleClick);
+
+  $("#note-2")
+    .dxDraggable({
+      onDragStart: handleDragStart,
+      onDragMove: handleDragMove,
+      onDragEnd: handleDragEnd,
+      group: "1",
+      boundary: ".board",
+    })
+    .on("click", handleClick);
+
+  $("#note-3")
+    .dxDraggable({
+      onDragStart: handleDragStart,
+      onDragMove: handleDragMove,
+      onDragEnd: handleDragEnd,
+      group: "1",
+      boundary: ".board",
+    })
+    .on("click", handleClick);
+
+  $("#note-4")
+    .dxDraggable({
+      onDragStart: handleDragStart,
+      onDragMove: handleDragMove,
+      onDragEnd: handleDragEnd,
+      boundary: ".board",
+      group: "1",
+
+    })
+    .on("click", handleClick);
 
     function changeZIndex(el) {
-        z++
-        el.css('z-index', z);
+        z++;
+        el.css("z-index", z);
     }
-    
+
     function handleClick(e) {
         changeZIndex($(e.currentTarget));
     }
-    
+
     function handleDragStart(e) {
         changeZIndex($(e.element[0]));
     }
-  
+
+    function handleDragMove(e) {
+        if (e.toComponent !== e.component) {
+            e.toComponent.element().css("opacity", "50%");
+        } else {
+            notes.forEach((el) => $(el).css('opacity', ''));
+        }
+    }
+
+    function handleDragEnd(e) {
+        e.toComponent.element().css("opacity", "");
+    }
 });

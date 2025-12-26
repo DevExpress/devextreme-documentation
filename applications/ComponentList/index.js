30c0fd79a203e298ac5e11fb4acc1e693447f625
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-26 14:18:55 +0400

    Remove Unnecessary Fluent Disable

--- applications/ComponentList/index.js
+++ applications/ComponentList/index.js
@@ -1,7 +1,1 @@
-Object.entries(document.styleSheets).forEach(([key, sheet]) => {
-    if (sheet.href?.includes('fluent')) {
-        document.styleSheets[key].disabled = true;
-    }
-});
-
 document.body.classList.add(localStorage.getItem('dx-devextreme-site-framework'));

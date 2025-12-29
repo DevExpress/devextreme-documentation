647dadb11f5c61d407e6d6a2e0c049f1e36f4f84
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-29 18:44:55 +0400

    Update Demo Container Code Following Example Improvements

--- applications/GettingStartedWith/Draggable/index.js
+++ applications/GettingStartedWith/Draggable/index.js
@@ -1,65 +1,68 @@
 $(() => {
-    const notes = ["#note-1", "#note-2", "#note-3", "#note-4"];
     let z = 1;
 
-    $("#note-1").dxDraggable({
-      onDragStart: handleDragStart,
-      onDragMove: handleDragMove,
-      onDragEnd: handleDragEnd,
-      boundary: ".board",
-      group: "1",
-    })
-    .on("click", handleClick);
+  $("#note-1").dxDraggable({
+    onDragStart: handleDragStart,
+    boundary: ".board",
+    group: "cards",
+  }).on({
+    "click": handleClick,
+    "dxdragenter": handleDragEnter,
+    "dxdragleave": handleDragStop,
+    "dxdrop": handleDragStop,
+  });
 
-    $("#note-2").dxDraggable({
-      onDragStart: handleDragStart,
-      onDragMove: handleDragMove,
-      onDragEnd: handleDragEnd,
-      group: "1",
-      boundary: ".board",
-    })
-    .on("click", handleClick);
+  $("#note-2").dxDraggable({
+    onDragStart: handleDragStart,
+    group: "cards",
+    boundary: ".board",
+  }).on({
+    "click": handleClick,
+    "dxdragenter": handleDragEnter,
+    "dxdragleave": handleDragStop,
+    "dxdrop": handleDragStop,
+  });
 
-    $("#note-3").dxDraggable({
-      onDragStart: handleDragStart,
-      onDragMove: handleDragMove,
-      onDragEnd: handleDragEnd,
-      group: "1",
-      boundary: ".board",
-    })
-    .on("click", handleClick);
+  $("#note-3").dxDraggable({
+    onDragStart: handleDragStart,
+    group: "cards",
+    boundary: ".board",
+  }).on({
+    "click": handleClick,
+    "dxdragenter": handleDragEnter,
+    "dxdragleave": handleDragStop,
+    "dxdrop": handleDragStop,
+  });
 
-    $("#note-4").dxDraggable({
-      onDragStart: handleDragStart,
-      onDragMove: handleDragMove,
-      onDragEnd: handleDragEnd,
-      boundary: ".board",
-      group: "1",
-    })
-    .on("click", handleClick);
+  $("#note-4").dxDraggable({
+    onDragStart: handleDragStart,
+    boundary: ".board",
+    group: "cards",
+  }).on({
+    "click": handleClick,
+    "dxdragenter": handleDragEnter,
+    "dxdragleave": handleDragStop,
+    "dxdrop": handleDragStop,
+  });
 
-    function changeZIndex(el) {
-        z++;
-        el.css("z-index", z);
-    }
+  function changeZIndex(el) {
+    z++;
+    el.css("z-index", z);
+  }
 
-    function handleClick(e) {
-        changeZIndex($(e.currentTarget));
-    }
+  function handleClick(e) {
+    changeZIndex($(e.currentTarget));
+  }
 
-    function handleDragStart(e) {
-        changeZIndex($(e.element[0]));
-    }
+  function handleDragStart(e) {
+    changeZIndex($(e.element[0]));
+  }
 
-    function handleDragMove(e) {
-        if (e.toComponent !== e.component) {
-            e.toComponent.element().css("outline", "1px dashed red");
-        } else {
-            notes.forEach((el) => $(el).css('outline', ''));
-        }
-    }
+  function handleDragEnter(e) {
+    $(e.target).css("outline", "1px dashed red");
+  }
 
-    function handleDragEnd(e) {
-        notes.forEach((el) => $(el).css('outline', ''));
-    }
+  function handleDragStop(e) {
+    $(e.target).css("outline", "");
+  }
 });

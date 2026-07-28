c4d6c2778df4cde57c0ed5d9c50ca534e559003a
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2026-07-28 12:57:58 +0400

    Icons Gallery: Add Minor Script Improvements (#9045)

--- applications/Icons/script.js
+++ applications/Icons/script.js
@@ -1,146 +1,148 @@
-﻿let mainSheet;
+﻿$(function() {
+    let mainSheet;
 
-for (const sheet of document.styleSheets) {
-    if (sheet.href?.includes('fluent')) {
-        mainSheet = sheet;
-        break;
-    }
-}
-
-const singleIconStyleRegex = /.dx-icon-([\w]+)::before/
-const doubleIconStyleRegex = /.dx-icon-([\w]+)::before, .dx-icon-([\w]+)::before/
-const tripleIconStyleRegex = /.dx-icon-([\w]+)::before, .dx-icon-([\w]+)::before, .dx-icon-([\w]+)::before/
-
-let icons = [];
-
-for (let id = 0; id < mainSheet.cssRules.length; id++) {
-    const rule = mainSheet.cssRules[id];
-
-    if (rule.selectorText && (rule.selectorText.match(singleIconStyleRegex) && !rule.selectorText.includes(' ') || rule.selectorText.match(doubleIconStyleRegex))) {
-        const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
-        const iconName = rule.selectorText.split('::')[0].split('-')[2];
-        icons.push({id, iconName, iconChar});
-    }
-
-    if (rule.selectorText && rule.selectorText.match(doubleIconStyleRegex)) {
-        const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
-        const iconName = rule.selectorText.split('::')[1].split('-')[2];
-        icons.push({id, iconName, iconChar});
-    }
-
-    if (rule.selectorText && rule.selectorText.match(tripleIconStyleRegex)) {
-        const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
-        const iconName = rule.selectorText.split('::')[2].split('-')[2];
-        icons.push({id, iconName, iconChar});
-    }
-}
-
-const themes = ['Fluent', 'Material', 'Generic'];
-
-function changeIconTheme(selectedTheme) {
-    for (const theme of themes) {
-        if (selectedTheme === theme) {
-            $('.icon-container i').addClass(`${theme}-icons`);
-            $('.dx-cardview-content.dx-cardview-content-grid .dx-button').addClass(`${theme}-icons`);
-        } else {
-            $('.icon-container i').removeClass(`${theme}-icons`);
-            $('.dx-cardview-content.dx-cardview-content-grid .dx-button').removeClass(`${theme}-icons`);
+    for (const sheet of document.styleSheets) {
+        if (sheet.href?.includes('fluent')) {
+            mainSheet = sheet;
+            break;
         }
     }
-}
 
-const tabs = $('#tabs').dxTabs({
-    dataSource: themes,
-    selectedItem: 'Fluent',
-    onSelectionChanged(data) {
-        const selectedTheme = data.addedItems[0];
-        changeIconTheme(selectedTheme);
-    },
-}).dxTabs('instance');
+    const singleIconStyleRegex = /\.dx-icon-([\w]+)::before/
+    const doubleIconStyleRegex = /\.dx-icon-([\w]+)::before, \.dx-icon-([\w]+)::before/
+    const tripleIconStyleRegex = /\.dx-icon-([\w]+)::before, \.dx-icon-([\w]+)::before, \.dx-icon-([\w]+)::before/
 
-function copyData(text) {
-    if (!navigator.clipboard) {
-        DevExpress.ui.notify({ contentTemplate() {
-                return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
-                    $('<i>').addClass('dx-icon-errorcircle'),
-                    $('<div>').text(`Failed to copy icon.`),
-                )
-            }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
-    } else {
-        navigator.clipboard.writeText(text).then(() => {
-            DevExpress.ui.notify({ contentTemplate() {
-                return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
-                    $('<i>').addClass('dx-icon-info'),
-                    $('<div>').text(`"${text}" copied to clipboard.`),
-                )
-            }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
-        }).catch((e) => {
-            DevExpress.ui.notify({ contentTemplate() {
-                return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
-                    $('<i>').addClass('dx-icon-errorcircle'),
-                    $('<div>').text(`Failed to copy icon: ${e}`),
-                )
-            }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
-        })
+    let icons = [];
+
+    for (let id = 0; id < mainSheet.cssRules.length; id++) {
+        const rule = mainSheet.cssRules[id];
+
+        if (rule.selectorText && (rule.selectorText.match(singleIconStyleRegex) && !rule.selectorText.includes(' ') || rule.selectorText.match(doubleIconStyleRegex))) {
+            const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
+            const iconName = rule.selectorText.split('::')[0].split('-')[2];
+            icons.push({id, iconName, iconChar});
+        }
+
+        if (rule.selectorText && rule.selectorText.match(doubleIconStyleRegex)) {
+            const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
+            const iconName = rule.selectorText.split('::')[1].split('-')[2];
+            icons.push({id, iconName, iconChar});
+        }
+
+        if (rule.selectorText && rule.selectorText.match(tripleIconStyleRegex)) {
+            const iconChar = `\\${rule.style.content.replace(/["']/g, '').codePointAt(0).toString(16)}`;
+            const iconName = rule.selectorText.split('::')[2].split('-')[2];
+            icons.push({id, iconName, iconChar});
+        }
     }
-}
 
-$('#cardview-container').dxCardView({
-    dataSource: icons,
-    columns: [{
-        dataField: 'iconName',
-        caption: 'Name',
-        sortOrder: 'asc',
-    }, {
-        dataField: 'iconChar',
-        caption: 'Character Code',
-    }],
-    cardContentTemplate(data) {
-        return $('<div class="icon-container">').append(
-            $(`<i class='icon dx-icon-${data.card.data.iconName}' style='text-align: center'></i>`),
-            $('<div class="icon-name">').dxButton({
-                stylingMode: 'text',
-                hoverStateEnabled: true,
-                activeStateEnabled: false,
-                focusStateEnabled: false,
-                text: data.card.data.iconName,
-                height: 24,
-                onClick(e) {
-                    copyData(data.card.data.iconName);
-                }
-            }),
-            $('<div class="icon-char">').dxButton({
-                stylingMode: 'text',
-                hoverStateEnabled: true,
-                activeStateEnabled: false,
-                focusStateEnabled: false,
-                text: data.card.data.iconChar,
-                height: 24,
-                onClick(e) {
-                    copyData(data.card.data.iconChar);
-                }
-            }),
-        )
-    },
-    cardsPerRow: 'auto',
-    searchPanel: {
-        visible: true,
-    },
-    scrolling: {
-        useNative: false,
-        scrollByThumb: true,
-        showScrollbar: 'onHover',
-    },
-    paging: {
-        enabled: false,
-    },
-    pager: {
-        visible: false,
-    },
-    height: 656,
-    cardMinWidth: 120,
-    onContentReady() {
-        const selectedTheme = tabs.option('selectedItem');
-        changeIconTheme(selectedTheme);
-    },
-});
+    const themes = ['Fluent', 'Material', 'Generic'];
+
+    function changeIconTheme(selectedTheme) {
+        for (const theme of themes) {
+            if (selectedTheme === theme) {
+                $('.icon-container i').addClass(`${theme}-icons`);
+                $('.dx-cardview-content.dx-cardview-content-grid .dx-button').addClass(`${theme}-icons`);
+            } else {
+                $('.icon-container i').removeClass(`${theme}-icons`);
+                $('.dx-cardview-content.dx-cardview-content-grid .dx-button').removeClass(`${theme}-icons`);
+            }
+        }
+    }
+
+    const tabs = $('#tabs').dxTabs({
+        dataSource: themes,
+        selectedItem: 'Fluent',
+        onSelectionChanged(data) {
+            const selectedTheme = data.addedItems[0];
+            changeIconTheme(selectedTheme);
+        },
+    }).dxTabs('instance');
+
+    function copyData(text) {
+        if (!navigator.clipboard) {
+            DevExpress.ui.notify({ contentTemplate() {
+                    return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
+                        $('<i>').addClass('dx-icon-errorcircle'),
+                        $('<div>').text(`Failed to copy icon.`),
+                    )
+                }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
+        } else {
+            navigator.clipboard.writeText(text).then(() => {
+                DevExpress.ui.notify({ contentTemplate() {
+                    return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
+                        $('<i>').addClass('dx-icon-checkmarkcircle').css('color', 'var(--dx-color-success)'),
+                        $('<div>').text(`"${text}" copied to clipboard.`),
+                    )
+                }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
+            }).catch((e) => {
+                DevExpress.ui.notify({ contentTemplate() {
+                    return $('<div>').css({display: 'flex', 'align-items': 'center'}).append(
+                        $('<i>').addClass('dx-icon-errorcircle'),
+                        $('<div>').text(`Failed to copy icon: ${e}`),
+                    )
+                }, displayTime: 1000, minWidth: 320, maxWidth: 320 }, { position: "bottom center", direction: "up-push" });
+            })
+        }
+    }
+
+    $('#cardview-container').dxCardView({
+        dataSource: icons,
+        columns: [{
+            dataField: 'iconName',
+            caption: 'Name',
+            sortOrder: 'asc',
+        }, {
+            dataField: 'iconChar',
+            caption: 'Character Code',
+        }],
+        cardContentTemplate(data) {
+            return $('<div class="icon-container">').append(
+                $(`<i class='icon dx-icon-${data.card.data.iconName}' style='text-align: center'></i>`),
+                $('<div class="icon-name">').dxButton({
+                    stylingMode: 'text',
+                    hoverStateEnabled: true,
+                    activeStateEnabled: false,
+                    focusStateEnabled: false,
+                    text: data.card.data.iconName,
+                    height: 24,
+                    onClick(e) {
+                        copyData(data.card.data.iconName);
+                    }
+                }),
+                $('<div class="icon-char">').dxButton({
+                    stylingMode: 'text',
+                    hoverStateEnabled: true,
+                    activeStateEnabled: false,
+                    focusStateEnabled: false,
+                    text: data.card.data.iconChar,
+                    height: 24,
+                    onClick(e) {
+                        copyData(data.card.data.iconChar);
+                    }
+                }),
+            )
+        },
+        cardsPerRow: 'auto',
+        searchPanel: {
+            visible: true,
+        },
+        scrolling: {
+            useNative: false,
+            scrollByThumb: true,
+            showScrollbar: 'onHover',
+        },
+        paging: {
+            enabled: false,
+        },
+        pager: {
+            visible: false,
+        },
+        height: 656,
+        cardMinWidth: 120,
+        onContentReady() {
+            const selectedTheme = tabs.option('selectedItem');
+            changeIconTheme(selectedTheme);
+        },
+    });
+})
